8fb566c4b7acdf3ecf35935a8c592d621ddf02cc
Hakim Boukerdoun <116846705+Iamhakim@users.noreply.github.com>
2023-01-15 08:47:54 +0000

    add javascript data variable for the quizz

---
+++ script.js
@@ -0,0 +1,35 @@
+'use district';
+
+const quizData = [
+  {
+    Question: 'which language runs in the browser ?',
+    a: 'java',
+    b: 'c',
+    d: 'python',
+    correct: 'd',
+  },
+  {
+    Question: 'what does CSS stand for ?',
+    a: 'Central Style Sheets',
+    b: 'Cascading Style Sheets',
+    c: 'Cascading Simple Sheets',
+    d: 'Cars SUVs Sailboats',
+    correct: 'b',
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Hypertext Markdown Language',
+    c: 'Hyperloop Machine Language',
+    d: 'Helicopters Terminals Motorboats Lamborginis',
+    correct: 'a',
+  },
+  {
+    question: 'What year was JavaScript launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: 'b',
+  },
+];
